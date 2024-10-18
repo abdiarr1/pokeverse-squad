3fcf343f5d2fed8f5c76df18fdeaa0d92407da8a
abdiarr1 <abdir.arr123@gmail.com>
2024-10-18 15:05:41 +0100

    change class to className

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -4,10 +4,10 @@ const Error = () => {
   return (
     <div className="p-10">
       <div
-        class="bg-red-100 border-l-4 border-red-500 text-orange-700 p-4"
+        className="bg-red-100 border-l-4 border-red-500 text-orange-700 p-4"
         role="alert"
       >
-        <p class="font-bold">Error</p>
+        <p className="font-bold">Error</p>
         <p>Sorry, there was an error...</p>
       </div>
     </div>
